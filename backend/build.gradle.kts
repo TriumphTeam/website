9216b5ba770c1ab464ca275ab604cb0b5630fe2d
Matt <matt.mmor@gmail.com>
2025-02-09 18:11:56 +0000

    init

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,36 +1,8 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import tasks.TailwindPrepare
-
 plugins {
-    id("backend.base")
-    id("io.ktor.plugin") version "2.3.10"
+    id("backend.parent")
 }
 
-application {
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=true")
-    mainClass.set("dev.triumphteam.backend.ApplicationKt")
-}
-
-dependencies {
-    implementation(projects.common)
-
-    implementation(libs.bundles.logger)
-    implementation(libs.bundles.ktor.client)
-    implementation(libs.bundles.ktor.server)
-    implementation(libs.bundles.database)
-
-    implementation(libs.caffeine)
-}
-
-tasks {
-
-    register<TailwindPrepare>("tailwindPrepare") {
-        srcDir.set(project.sourceSets.main.get().kotlin.srcDirs.first())
-        resourcesDir.set(project.sourceSets.main.get().resources.srcDirs.first())
-        outputDir.set(rootDir.resolve("tailwind"))
-    }
-
-    withType<ShadowJar> {
-        archiveFileName.set("backend.jar")
-    }
+/** Used by CI to get the latest [project#version]. */
+tasks.register("ciVersion") {
+    println(project.version)
 }
--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("backend.parent")
-}
-
-/** Used by CI to get the latest [project#version]. */
-tasks.register("ciVersion") {
-    println(project.version)
-}
